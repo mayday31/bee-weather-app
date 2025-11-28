3eac16a451095f43cda658670e806e3189ea93e9
mayday31 <brianajlynn@gmail.com>
2025-11-28 15:23:02 -0500

    Added current weather conditions and the day/time

--- source/script.js
+++ source/script.js
@@ -2,9 +2,15 @@ function pullWeatherData(response) {
   let tempHeader = document.querySelector("#current-temp-value");
   let temperature = Math.round(response.data.temperature.current);
   let currentCity = document.querySelector("#current-city");
+  let condition = document.querySelector("#weather-condition");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
 
-  currentCity.innerHTML = response.data.city;
   tempHeader.innerHTML = temperature;
+  currentCity.innerHTML = response.data.city;
+  condition.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed}mph`;
 }
 
 function searchCity(city) {
@@ -19,6 +25,28 @@ function searchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+let currentDate = document.querySelector("#day-time");
+let realTime = new Date();
+let hours = realTime.getHours();
+let minutes = realTime.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[realTime.getDay()];
+
+currentDate.innerHTML = `${day} ${hours}:${minutes}`;
 
 let searchForm = document.querySelector("#form");
 searchForm.addEventListener("submit", searchSubmit);
